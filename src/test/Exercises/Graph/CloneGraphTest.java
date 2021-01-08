7b96f112a70fe847ca0dd2e2f3c091d96c383388
Alejandro Carlstein <acarlstein@iqn.com>
2021-01-08 14:46:17 -0700

    AC_01042021 Starting with Graph Clone

---
+++ src/test/Exercises/Graph/CloneGraphTest.java
@@ -0,0 +1,83 @@
+package Exercises.Graph;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class CloneGraphTest {
+
+    @Test
+    public void createGraphNode(){
+        GraphNode gn = new GraphNode();
+        assertEquals(gn.value, 0);
+        assertThat(gn.neighbords, instanceOf(List.class));
+        assertEquals(gn.neighbords.size(), 0);
+    }
+
+    @Test
+    public void createGraphNodeWithValue(){
+        GraphNode gn = new GraphNode(1);
+        assertEquals(gn.value, 1);
+        assertThat(gn.neighbords, instanceOf(List.class));
+        assertEquals(gn.neighbords.size(), 0);
+    }
+
+    @Test
+    public void createGraphNodeWithValueAndNeibords(){
+        List<GraphNode> list = new ArrayList<>();
+        GraphNode newNode = new GraphNode(2);
+        list.add(newNode);
+
+        GraphNode gn = new GraphNode(1, list);
+        assertEquals(gn.value, 1);
+        assertThat(gn.neighbords, instanceOf(List.class));
+        assertEquals(gn.neighbords.size(), 1);
+        assertEquals(gn.neighbords.get(0), newNode);
+    }
+
+    @Test
+    public void cannotCloneIfNull(){
+        assertNull(clone(null));
+    }
+
+    @Test
+    public void cloneGraphWithOneEdge(){
+        GraphNode graphNode = new GraphNode(1);
+        GraphNode graphClone = clone (graphNode);
+        assertNotSame(graphNode, graphClone);
+    }
+
+    static class GraphNode  {
+        public int value;
+        public List<GraphNode> neighbords;
+
+        public GraphNode(){
+            this(0);
+        }
+
+        public GraphNode(int value){
+            this(value, new ArrayList<GraphNode>());
+        }
+
+        public GraphNode(int value, List<GraphNode> neighbords){
+            this.value = value;
+            this.neighbords = neighbords;
+        }
+    }
+
+    static public GraphNode clone(GraphNode graphNode){
+        if (graphNode == null) return null;
+        GraphNode copy = new GraphNode(graphNode.value);
+        return copy;
+    }
+
+
+}
