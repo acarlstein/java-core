831b3976aa44ea7ea421da065d6e7b5197bbc8c6
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-01-08 20:21:09 -0700

    AC_01042021 Clone Graph Test

--- src/test/Exercises/Graph/CloneGraphTest.java
+++ src/test/Exercises/Graph/CloneGraphTest.java
@@ -3,16 +3,14 @@ package Exercises.Graph;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class CloneGraphTest {
 
@@ -58,7 +56,7 @@ public class CloneGraphTest {
     }
 
     @Test
-    public void cloneGraphNodeWithOneNeighbor(){
+    public void cloneGraphWithOneNeighbor(){
         GraphNode neighbor = new GraphNode(2);
         GraphNode graphNode = new GraphNode(1);
         graphNode.neighbords.add(neighbor);
@@ -68,6 +66,64 @@ public class CloneGraphTest {
         assertEquals(graphNode.value, graphClone.value);
         assertNotSame(graphNode.neighbords.get(0), graphClone.neighbords.get(0));
         assertEquals(graphNode.neighbords.get(0).value, graphClone.neighbords.get(0).value);
+    }
+
+    @Test
+    public void cloneGraphMultipleNeighborsTest(){
+        GraphNode graph = createDefaultGraph();
+        GraphNode clone = clone(graph);
+        assertNotSame(graph, clone);
+
+        Queue<GraphNode> graphQueue = new LinkedList<>();
+        Queue<GraphNode> cloneQueue = new LinkedList<>();
+
+        graphQueue.add(graph);
+        cloneQueue.add(clone);
+
+        Queue<GraphNode> discovered = new LinkedList<>();
+        discovered.add(graph);
+        discovered.add(clone);
+
+        while(!graphQueue.isEmpty() && !cloneQueue.isEmpty()){
+            GraphNode source = graphQueue.remove();
+            GraphNode target = cloneQueue.remove();
+            assertEquals(source.value, target.value);
+            for(int i = 0; i < source.neighbords.size(); ++i){
+                GraphNode sourceNeighbor = source.neighbords.get(i);
+                if (!discovered.contains(sourceNeighbor)){
+                    discovered.add(sourceNeighbor);
+                    graphQueue.add(sourceNeighbor);
+                }
+                GraphNode targetNeighbor = target.neighbords.get(i);
+                if (!discovered.contains(targetNeighbor)){
+                    discovered.add(targetNeighbor);
+                    cloneQueue.add(targetNeighbor);
+                }
+            }
+        }
+
+
+    }
+
+    private GraphNode createDefaultGraph(){
+        GraphNode graph = new GraphNode(1);
+        GraphNode node2 = new GraphNode(2);
+        GraphNode node3 = new GraphNode(3);
+        GraphNode node4 = new GraphNode(4);
+
+        graph.neighbords.add(node2);
+        graph.neighbords.add(node4);
+
+        node2.neighbords.add(node3);
+        node2.neighbords.add(graph);
+
+        node3.neighbords.add(node2);
+        node3.neighbords.add(node4);
+
+        node4.neighbords.add(node3);
+        node4.neighbords.add(graph);
+
+        return graph;
     }
 
     static class GraphNode  {
@@ -94,15 +150,19 @@ public class CloneGraphTest {
         return clone(graphNode, map);
     }
 
-    // We don't want to copy a node if we already have it
+
     static private GraphNode clone(GraphNode graphNode, Map<Integer, GraphNode> map){
+        // We don't want to create a copy if we already have it
         if (map.containsKey(graphNode.value)){
             return map.get(graphNode.value);
         }
+
         GraphNode copy = new GraphNode(graphNode.value);
         map.put(graphNode.value, copy);
+
+        // DFS
         for(GraphNode neighbor : graphNode.neighbords){
-            copy.neighbords.add(clone(neighbor));
+            copy.neighbords.add(clone(neighbor, map));
         }
         return copy;
     }
