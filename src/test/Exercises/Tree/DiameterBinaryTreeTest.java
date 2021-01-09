297c23025b78df96ccdccd81dd20485b5e869960
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-01-09 14:30:52 -0700

    AC_01092021 Count Nodes for Diameter of Binary Tree

--- src/test/Exercises/Tree/DiameterBinaryTreeTest.java
+++ src/test/Exercises/Tree/DiameterBinaryTreeTest.java
@@ -3,6 +3,8 @@ package Exercises.Tree;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.assertEquals;
 
 public class DiameterBinaryTreeTest {
@@ -37,7 +39,7 @@ public class DiameterBinaryTreeTest {
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
-        assertEquals(getDiameter(root), 2);
+        assertEquals(getDiameter(root), 3);
     }
 
     @Test
@@ -56,11 +58,45 @@ public class DiameterBinaryTreeTest {
         assertEquals(getDiameter(root), 3);
     }
 
+    @Test
+    public void DiameterOfTreeFirstCase(){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.right = new Node(3);
+        assertEquals(getDiameter(root), 4);
+    }
+
+    @Test
+    public void DiameterOfTreeSecondCase(){
+        Node root = new Node(1);
+        root.right = new Node(3);
+        root.right.left = new Node(4);
+        root.right.left.right = new Node(6);
+        root.right.left.right.right = new Node(10);
+        root.right.right = new Node(5);
+        root.right.right.left  = new Node(7);
+        assertEquals(getDiameter(root), 6);
+    }
+
+    @Test
+    public void DiameterOfTreeThirdCase(){
+
+    }
+
     public int getDiameter(Node root){
+        AtomicInteger countNodes = new AtomicInteger(0);
+        getDiameter(root, countNodes);
+        return countNodes.get();
+    }
+
+    private synchronized int getDiameter(Node root, AtomicInteger countNodes){
         if (root == null) return 0;
-        int leftHeight = getDiameter(root.left);
-        int rightHeight = getDiameter(root.right);
-        return Math.max(leftHeight, rightHeight) + 1;
+        int leftNodes = getDiameter(root.left, countNodes);
+        int rightNodes = getDiameter(root.right, countNodes);
+        int numberOfNodes = leftNodes + rightNodes + 1;
+        countNodes.set(Math.max(countNodes.get(), numberOfNodes));
+        return Math.max(leftNodes, rightNodes) + 1;
     }
 
     class Node {
